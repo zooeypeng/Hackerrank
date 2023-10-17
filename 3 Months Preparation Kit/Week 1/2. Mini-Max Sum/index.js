281d1475eb288347049c0ae1a89f092d31e862bf
zooeypeng <zooey.peng@gmail.com>
2023-10-17 20:38:58 +0800

    add solution for Mini-Max Sum

---
+++ 3 Months Preparation Kit/Week 1/2. Mini-Max Sum/index.js
@@ -0,0 +1,44 @@
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-mini-max-sum
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+    arr.sort((a, b) => a - b);
+    const mid = arr[1] + arr[2] + arr[3]
+    
+    console.log(arr[0]+mid, arr[4]+mid)
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
