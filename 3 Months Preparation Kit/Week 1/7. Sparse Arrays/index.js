9d61211b8dfe727ad800417edc46ac804fd89fa6
zooeypeng <zooey.peng@gmail.com>
2023-10-22 19:52:22 +0800

    add solution for Sparse Arrays.

---
+++ 3 Months Preparation Kit/Week 1/7. Sparse Arrays/index.js
@@ -0,0 +1,80 @@
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-sparse-arrays
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY strings
+ *  2. STRING_ARRAY queries
+ */
+
+function matchingStrings(strings, queries) {
+    // Write your code here
+    const occurrences = {}
+    
+    for (const string of strings) {
+        occurrences[string] = (occurrences[string] || 0) + 1;
+    }
+    
+    
+    const results = []
+    
+    for (const query of queries) {
+        results.push(occurrences[query] || 0)
+    }
+    
+    return results
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const stringsCount = parseInt(readLine().trim(), 10);
+
+    let strings = [];
+
+    for (let i = 0; i < stringsCount; i++) {
+        const stringsItem = readLine();
+        strings.push(stringsItem);
+    }
+
+    const queriesCount = parseInt(readLine().trim(), 10);
+
+    let queries = [];
+
+    for (let i = 0; i < queriesCount; i++) {
+        const queriesItem = readLine();
+        queries.push(queriesItem);
+    }
+
+    const res = matchingStrings(strings, queries);
+
+    ws.write(res.join('\n') + '\n');
+
+    ws.end();
+}
