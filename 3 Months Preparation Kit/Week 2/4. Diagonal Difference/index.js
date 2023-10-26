01082f823f9a5abeb6d4dbc77a2b2c23055e69f4
zooeypeng <zooey.peng@gmail.com>
2023-10-26 21:31:41 +0800

    add solution for Diagonal Difference.

---
+++ 3 Months Preparation Kit/Week 2/4. Diagonal Difference/index.js
@@ -0,0 +1,64 @@
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-diagonal-difference
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference(arr) {
+    // Write your code here
+    const n = arr.length;
+    let leftSum = 0;
+    let rightSum = 0;
+    
+    for (let i = 0; i < n; i++) {
+        leftSum += arr[i][i];
+        rightSum += arr[i][n - i - 1];
+    }
+    
+    return Math.abs(leftSum - rightSum);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
