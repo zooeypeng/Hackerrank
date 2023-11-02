3f93d0dfe04c38b8ba235e1880d712067a04d756
zooeypeng <zooey.peng@gmail.com>
2023-11-02 22:19:41 +0800

    add solution for Sales by Match.

---
+++ 3 Months Preparation Kit/Week 3/4. Sales by Match/index.js
@@ -0,0 +1,61 @@
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-sock-merchant
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+    // Write your code here
+    const sockCounts = {};
+     
+    for (const color of ar) {
+        sockCounts[color] = (sockCounts[color] || 0) + 1;
+    }
+     
+    const totalPair = Object.values(sockCounts).reduce((acc, curr) => acc + Math.floor(curr / 2), 0);
+     
+    return totalPair;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = sockMerchant(n, ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
