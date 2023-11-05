935b3b72fa634767ad6ee9af76b3cacbb99c8372
zooeypeng <zooey.peng@gmail.com>
2023-11-05 20:55:26 +0800

    add solution for Between Two Sets.

---
+++ 3 Months Preparation Kit/Week 3/Mock Test: Between Two Sets/index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function getTotalX(a, b) {
+    // Write your code here
+    let count = 0;
+    
+    const maxA = Math.max(...a);
+    const minB = Math.min(...b);
+    
+    
+    for (let i = maxA; i <= minB; i++) {
+        if (a.every(num => i % num === 0) && b.every(num => num % i === 0)) {
+            count++
+        }
+    }
+    
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const brr = readLine().replace(/\s+$/g, '').split(' ').map(brrTemp => parseInt(brrTemp, 10));
+
+    const total = getTotalX(arr, brr);
+
+    ws.write(total + '\n');
+
+    ws.end();
+}
