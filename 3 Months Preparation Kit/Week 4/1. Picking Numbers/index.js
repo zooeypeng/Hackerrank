9ca21100e8fab8e8af3324c08384699bb9e414cb
zooeypeng <zooey.peng@gmail.com>
2023-11-06 21:29:40 +0800

    add solution for Picking Numbers.

---
+++ 3 Months Preparation Kit/Week 4/1. Picking Numbers/index.js
@@ -0,0 +1,59 @@
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-picking-numbers
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function pickingNumbers(a) {
+    // Write your code here
+    let maxCount = 0;
+    
+    for (let num of a) {
+        let count = a.filter(x => x === num || x === num + 1 ).length;
+        
+        maxCount = Math.max(count, maxCount)
+    }
+    
+    return maxCount
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = pickingNumbers(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
