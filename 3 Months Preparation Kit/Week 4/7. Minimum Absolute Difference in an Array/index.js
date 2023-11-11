70e625d7f777734410b247b5e650cdba496d8c07
zooeypeng <zooey.peng@gmail.com>
2023-11-11 20:36:59 +0800

    add solution for Minimum Absolute Difference in an Array

---
+++ 3 Months Preparation Kit/Week 4/7. Minimum Absolute Difference in an Array/index.js
@@ -0,0 +1,59 @@
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-minimum-absolute-difference-in-an-array
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'minimumAbsoluteDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function minimumAbsoluteDifference(arr) {
+    // Write your code here
+    arr.sort((a, b) => a - b);
+    
+    let minDiff = Infinity;
+    
+    for (let i = 1; i < arr.length; i++) {
+        minDiff = Math.min(Math.abs(arr[i] - arr[i - 1]), minDiff);
+    }
+    
+    return minDiff;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = minimumAbsoluteDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
