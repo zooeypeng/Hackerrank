3b208aa59719e8e756ddeefa75fad0aa024fe316
zooeypeng <zooey.peng@gmail.com>
2023-11-22 19:37:50 +0800

    add solution for Sherlock and Array.

---
+++ 3 Months Preparation Kit/Week 6/2. Sherlock and Array/index.js
@@ -0,0 +1,66 @@
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-sherlock-and-array
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'balancedSums' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function balancedSums(arr) {
+    // Write your code here
+    let totalSum = arr.reduce((sum, num) => sum + num, 0);
+    let leftSum = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+        totalSum -= arr[i];
+        if (totalSum === leftSum) {
+            return 'YES';
+        }
+        leftSum += arr[i]
+    }
+    
+    return 'NO';
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const T = parseInt(readLine().trim(), 10);
+
+    for (let TItr = 0; TItr < T; TItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+        const result = balancedSums(arr);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
