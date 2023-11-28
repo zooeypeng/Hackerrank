992380009b3236539faf4965a6901eea49d5dd08
zooeypeng <zooey.peng@gmail.com>
2023-11-28 21:20:04 +0800

    add solution for Palindrom Index.

---
+++ 3 Months Preparation Kit/Week 6/Mock Test: Palindrom Index/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+/*
+ * Complete the 'palindromeIndex' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+function palindromeIndex(s) {
+    // Write your code here
+    const isPalindromIndex = (str) => str === str.split('').reverse().join('');
+    
+    for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+        if (s[i] !== s[j]) {
+            return isPalindromIndex(s.substring(0, i) + s.substring(i + 1)) ? i : j;
+        }
+    }
+    
+    return -1;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        const result = palindromeIndex(s);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
